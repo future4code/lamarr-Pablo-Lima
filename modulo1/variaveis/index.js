1fce22c5e9e0b7d1c81da9769e992e200ac5d294
PabloBorek <pabloborek@hotmail.com>
2022-04-12 19:30:38 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,39 @@
+//Exercícios de interpretação de código
+/*let a = 10
+let b = 10
+
+console.log(10)
+
+b = 5
+console.log(10,5)
+
+
+let a = 10
+let b = 20
+c = a
+b = c10
+a = b
+
+console.log(10, 10, 10)
+
+let p = prompt("8")
+let t = prompt("70")
+alert(`Voce recebe ${t/p} por hora`)*/
+const nome = 'Pablo Henrique'
+let idade = 18
+typeof nome
+typeof idade
+//ele imprimiu as variaveis q eu coloquei
+console.log(prompt('qual o seu nome e sua idade?'))
+//console.log('ola', nome, 'vc tem', idade, 'anos')
+
+const estudante = true
+
+console.log(
+  'meu nome eh',
+  nome,
+  'e tenho',
+  idade,
+  'anos e no momento estou estudando?',
+  estudante
+)
